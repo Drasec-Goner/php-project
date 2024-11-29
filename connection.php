53635cceda60bd422ebf413885f0ee130e4e472a
Soumya Manna <drasec.04@gmail.com>
2024-11-29 13:13:09 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,6 +1,6 @@
 <?php
 
-	$conn = mysqli_connect('cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306','j6ug5pjglhww1k9c','edabzuwp2xphxj81','n71yrgnfjla5oyrd');
+	$conn = mysqli_connect('cvktne7b4wbj4ks1.chr7pe7iynqr.eu-west-1.rds.amazonaws.com','j6ug5pjglhww1k9c','edabzuwp2xphxj81','n71yrgnfjla5oyrd');
 
 	if(!$conn)
 		die("Error while connecting...!").mysqli_connect_error($conn);
